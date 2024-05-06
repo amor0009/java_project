317841be1ae65fd4215d43a302660b4af86e26e4
amor <159952777+amor0009@users.noreply.github.com>
2024-05-06 15:48:06 +0300

    update

--- src/main/java/com/mangadex/lab2/controller/AuthorController.java
+++ src/main/java/com/mangadex/lab2/controller/AuthorController.java
@@ -17,35 +17,36 @@ import java.util.List;
 public class AuthorController {
     private AuthorService authorService;
     private CounterService counterService;
+    private static final String INCREMENT = "Incremented COUNTER to  ";
 
     @GetMapping(value = "list")
     public List<Author> getAllAuthors() {
-        log.info("Incremented COUNTER to " + String.valueOf(counterService.incrementAndGetCount()));
+        log.info(INCREMENT + CounterService.incrementAndGetCount());
         return authorService.getAllAuthors();
     }
 
     @GetMapping(value = "find/{authorId}", produces = "application/json")
     public ResponseEntity<Author> getAuthorById(@PathVariable String authorId) {
-        log.info("Incremented COUNTER to " + String.valueOf(counterService.incrementAndGetCount()));
+        log.info(INCREMENT + CounterService.incrementAndGetCount());
         return ResponseEntity.ok(authorService.getAuthorById(authorId));
     }
 
     @PostMapping("save")
     public ResponseEntity<Author> addAuthor(@RequestBody Author author) {
-        log.info("Incremented COUNTER to " + String.valueOf(counterService.incrementAndGetCount()));
+        log.info(INCREMENT + CounterService.incrementAndGetCount());
         return new ResponseEntity<>(authorService.addAuthor(author), HttpStatus.OK);
     }
 
     @PutMapping("update")
     public ResponseEntity<Author> updateAuthor(@RequestBody Author author) {
-        log.info("Incremented COUNTER to " + String.valueOf(counterService.incrementAndGetCount()));
+        log.info(INCREMENT + CounterService.incrementAndGetCount());
         return new ResponseEntity<>(authorService.updateAuthor(author), HttpStatus.OK);
     }
 
     @DeleteMapping("delete/{authorId}")
     public HttpStatus deleteAuthor(@PathVariable String authorId) {
         authorService.deleteAuthorById(authorId);
-        log.info("Incremented COUNTER to " + String.valueOf(counterService.incrementAndGetCount()));
+        log.info(INCREMENT + CounterService.incrementAndGetCount());
         return HttpStatus.OK;
     }
 }

9072fea56a1169aae9123a92ed6fd749d37ad756
amor <159952777+amor0009@users.noreply.github.com>
2024-05-06 14:43:19 +0300

    update

---
+++ src/main/java/com/mangadex/lab2/service/CounterService.java
@@ -0,0 +1,13 @@
+package com.mangadex.lab2.service;
+
+import org.springframework.stereotype.Service;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+public class CounterService {
+    private static final AtomicInteger COUNTER = new AtomicInteger(0);
+
+    public static synchronized int incrementAndGetCount() {
+        return COUNTER.incrementAndGet();
+    }
+}
